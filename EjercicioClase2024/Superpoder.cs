d8a316e30fb30d7f6f2bf42b1ed870fa66ad097d
JohanVasquezM <vasquezmaldonadojohanisaac@gmail.com>
2024-05-01 17:25:44 -0600

    Agregando atributos de Super Héroe

---
+++ EjercicioClase2024/Superpoder.cs
@@ -0,0 +1,16 @@
+﻿namespace EjercicioClase2024
+{
+    public class Superpoder
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Nivel { get; set; }
+
+        public Superpoder(string nombre, string descripcion, int nivel)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Nivel = nivel;
+        }
+    }
+}
