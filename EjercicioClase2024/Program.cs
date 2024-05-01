05a4a28d2626c5889c5098a279df408178adf42e
JohanVasquezM <vasquezmaldonadojohanisaac@gmail.com>
2024-05-01 17:23:08 -0600

    Agregando los datos de los Super Héroes

--- EjercicioClase2024/Program.cs
+++ EjercicioClase2024/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioClase2024;
+{
+    {
+        SuperHeroe Mujer_Maravilla = new SuperHeroe(
+            "Mujer Maravilla",
+            "Diana Prince",
+            "Themyscira",
+            false,
+            new Superpoder("Super fuerza y agilidad", "Capacidad de levanta objetos muy pesados y agilidad sobrehumana", 9));
+
+        SuperHeroe Batman = new SuperHeroe(
+            "Batman",
+            "Bruce Wayne",
+            "Gotham",
+            false,
+            new Superpoder("Inteligencia superior","Capacidad de resolver problemas complejos y estrategia", 7));
+
+        SuperHeroe Flash = new SuperHeroe(
+            "Flash",
+            "Barry Allen",
+            "Central City",
+            false,
+            new Superpoder("Super velocidad", "Capacidad de moverse a velocidades inimaginables", 10));
+
+        Mujer_Maravilla.MostrarInformacion();
+        Console.WriteLine();
+        Batman.MostrarInformacion();
+        Console.WriteLine();
+        Flash.MostrarInformacion();
+    }
+}
